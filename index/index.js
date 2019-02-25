e8b6ae71bbb653ab936d0ad424c6c6e9dc2578b1
drosser <drosser@users.noreply.github.com>
2019-02-25 17:29:13 +0000

    Add info to homepage when user has registered

--- index/index.js
+++ index/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {messages: req.flash('info')});
 });
 
 module.exports = router;
