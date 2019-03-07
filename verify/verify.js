48b2c318959d116ce65e5f40ae7103ba43b4e3e3
drosser <drosser@users.noreply.github.com>
2019-03-07 14:04:28 +0000

    Add verification process

---
+++ verify/verify.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const router = express.Router();
+const config =  require('../config/config.js');
+
+const dbname = 'people';
+
+const mongojs = require('mongojs');
+const db = mongojs('curious', [dbname]);
+
+const aws = require('aws-sdk');
+
+aws.config.update({region: 'eu-west-1'});
+
+router.get('/:email', function(req, res) {
+
+    //get email to verify
+    var decode = req.params;
+
+    //decode the string from base64 encoded
+    var decoded = Buffer.from(decode.email, 'base64').toString('utf8');
+
+    //get signature
+    var decodedSignature = decoded.slice(-Math.abs(config.verify.signature.length));
+    var decodedEmail = decoded.slice(0, decoded.length - config.verify.signature.length);
+
+    if (decodedEmail.length == 0) {
+        console.log('decodedEmail is empty');
+        res.redirect('/');
+    }
+
+    if (decodedSignature.length == 0) {
+        console.log('decodedSignature is empty');
+        res.redirect('/');
+    }
+
+    if (decodedSignature == config.verify.signature) {
+        verifyEmail(decodedEmail).then(function() {
+            var params = {
+                Destination: {
+                    ToAddresses: [
+                        decodedEmail
+                    ]
+                },
+                Message: {
+                    Body: {
+                        Html: {
+                            Charset: 'UTF-8',
+                            Data: '<p>Congratulations! You have signed up for #CuriousCoffee. You will receive an email in due course matching you for your #CuriousCoffee</p>'
+                        }
+                    },
+                    Subject: {
+                        Charset: 'UTF-8',
+                        Data: 'Sign up confirmed!'
+                    }
+                },
+                Source: 'curious-coffee@companieshouse.gov.uk'
+            };
+
+            console.log('Sending email to address ' + decodedEmail);
+        
+            var sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+        
+            sendPromise.then(function(data) {
+                console.log(data.MessageId);
+        
+                req.flash('info', 'Congratulations! You have signed up for #CuriousCoffee. You will receive an email in due course matching you for your #CuriousCoffee');
+                res.redirect('/');
+            }).catch(function(err) {
+                console.error(err, err.stack);
+            });
+        });
+    } 
+
+    res.redirect('/');
+});
+
+function verifyEmail(decodedEmail) {
+    return new Promise(function(resolve, reject) {
+        db.people.update({
+            "email": decodedEmail
+        }, {"$set": {"verify": true}}, function() {
+
+        });
+
+        resolve();
+    });
+};
+
+module.exports = router;
