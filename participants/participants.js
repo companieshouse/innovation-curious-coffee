24748fd43f77aacc22b1113b69e2bdbacf0925c5
drosser <drosser@users.noreply.github.com>
2019-03-13 17:01:13 +0000

    Update participants to use config and redirect to error on error

--- participants/participants.js
+++ participants/participants.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const middleware = require('../middleware/middleware');
-
-const dbname = 'people';
-
+const config = require('../config/config');
 const mongojs = require('mongojs');
-const db = mongojs('curious', [dbname]);
+
+const db = mongojs(config.db.name, config.db.collections);
 
 router.get('/', middleware, function(req, res) {
 
     db.people.find().sort({department:1}, function(err, docs) {
+
+        if (err) {
+            res.redirect('/error');
+        }
+
         res.render('participants', {participants: docs});
     });
 });
