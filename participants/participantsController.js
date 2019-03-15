d71098b2e07c336db9071420757b4039948fb3fd
drosser <drosser@users.noreply.github.com>
2019-03-15 12:28:23 +0000

    Update participants to use mongoose

---
+++ participants/participantsController.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const middleware = require('../middleware/middleware');
+
+const ParticipantsService = require('./participantsService');
+
+router.get('/', middleware, function(req, res) {
+
+    var participantsService = new ParticipantsService();
+
+    participantsService.getParticipants(function(err, participants) {
+        if (err) {
+            console.error(err);
+            return res.redirect('/oops');
+        }
+
+        res.render('participants', {
+           participants: participants 
+        });
+    });
+});
+
+module.exports = router;
