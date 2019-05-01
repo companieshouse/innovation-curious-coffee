e57b84925c50ac7fb577a4d2e9bdefbac7089b97
drosser <drosser@users.noreply.github.com>
2019-05-01 14:31:29 +0100

    Add live statistics to index

--- index/index.js
+++ index/index.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const Participant = require('../models/participant');
 
-router.get('/', (req, res) => {
-    res.render('index', {messages: req.flash('info')});
-});
+router.get('/', get);
+
+async function get(req, res) {
+    let numParticipants = await getParticipantCount();
+    let numMatches = await getMatchCount();
+
+    res.render('index', {
+        numParticipants: numParticipants, 
+        numMatches: numMatches, 
+        messages: req.flash('info')
+    });
+}
+
+function getParticipantCount() {
+    return Participant.countDocuments({verify: true});
+}
+
+function getParticipantsMatches() {
+    return Participant.find({verify: true, matches: {$exists: true}});
+}
+
+async function getMatchCount() {
+    let participants = await getParticipantsMatches();
+
+    let count = 0;
+
+    participants.forEach(element => {
+        count += element.matches.length;
+    });
+
+    return await count / 2;
+}
 
 module.exports = router;
