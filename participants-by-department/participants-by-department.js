888aef924b66f8bca0ee0222db737d0a826509b0
drosser <drosser@users.noreply.github.com>
2019-05-01 16:04:17 +0100

    Update to use async/await and remove admin middleware

--- participants-by-department/participants-by-department.js
+++ participants-by-department/participants-by-department.js
@@ -1,78 +1,70 @@
 var express = require('express');
 var router = express.Router();
-const middleware = require('../middleware/middleware');
 const Participant = require('../models/participant');
 
-router.get('/', middleware, function(req, res) {
+router.get('/', function(req, res) {
     res.render('participants_department_chart');
 });
 
-router.get('/data', middleware, function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
+router.get('/data', getData);
 
-    getParticipantsByDepartment(function(err, docs) {
-        if (err) {
-            console.error(err);
-            return res.redirect('/oops');
-        }
+async function getData(req, res) {
+    let participantsByDepartmentData = await getParticipantsByDepartment();
 
-        Promise.all(docs).then(function() {
-            var labels = [];
-            var data = [];
-            var bgColor = [];
+    var labels = [];
+    var data = [];
+    var bgColor = [];
 
-            docs.forEach(function(doc) {
-                labels.push(doc._id);
-                data.push(doc.count);
-                bgColor.push("rgba(" + randomIntFromInterval(0,255) + ", " + randomIntFromInterval(0,255) + ", " + randomIntFromInterval(0,255) + ", 0.2)");
-            });
-
-            Promise.all(labels, data, bgColor).then(function() {
-                return res.send({
-                    type: 'bar',
-                    data: {
-                        labels: labels,
-                        datasets: [{
-                            data: data,
-                            backgroundColor: bgColor,
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        title: {     
-                            display: true,
-                            text: "Number of participants by department"
-                        },
-                        legend: {
-                            display: false
-                        },
-                        scales: {
-                            yAxes: [{
-                                    display: true,
-                                ticks: {
-                                    beginAtZero: true,
-                                    stepValue: 10,
-                                    stepSize: 1
-                                }
-                                }],
-                            xAxes: [{
-                                ticks: {
-                                    autoSkip: false
-                                }
-                            }]
-                        }
-                    }
-                });
-            });
-        });
+    participantsByDepartmentData.forEach(element => {
+        labels.push(element._id);
+        data.push(element.count);
+        bgColor.push("rgba(" + randomIntFromInterval(0,255) + ", " + randomIntFromInterval(0,255) + ", " + randomIntFromInterval(0,255) + ", 0.2)");
     });
-});
 
-function randomIntFromInterval(min,max) {
+    await labels, data, bgColor;
+
+    return res.send({
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: bgColor,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            title: {     
+                display: true,
+                text: "Number of participants by department"
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                        display: true,
+                    ticks: {
+                        beginAtZero: true,
+                        stepValue: 10,
+                        stepSize: 1
+                    }
+                    }],
+                xAxes: [{
+                    ticks: {
+                        autoSkip: false
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-function getParticipantsByDepartment(callback) {
+function getParticipantsByDepartment() {
     return Participant.aggregate([{
         "$group": {
             _id: "$department",
@@ -80,7 +72,7 @@ function getParticipantsByDepartment(callback) {
                 "$sum": 1
             }
         }
-    }], callback);
+    }]);
 };
 
 module.exports = router;
