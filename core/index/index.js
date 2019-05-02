0c219477a4e0d3995c71ec1d16c75a30ee696799
drosser <drosser@users.noreply.github.com>
2019-05-02 15:51:35 +0100

    Add missing index to core folder

--- core/index/index.js
+++ core/index/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Participant = require('../models/participant');
+const Participant = require('../../models/participant');
 
 router.get('/', get);
 
