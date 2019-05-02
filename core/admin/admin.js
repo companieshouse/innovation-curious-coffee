e0a18bcb8178ab93c54adf5511bd65afb8fa3610
drosser <drosser@users.noreply.github.com>
2019-05-02 15:02:28 +0100

    Move core modules to core folder

--- core/admin/admin.js
+++ core/admin/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const config = require('../config/config');
+const config = require('../../config/config');
 
 router.get('/', function(req, res, next) {
     res.render('admin');
--- core/participants/participantsController.js
+++ core/participants/participantsController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const middleware = require('../middleware/middleware');
+const middleware = require('../../middleware/middleware');
 
 const ParticipantsService = require('./participantsService');
 
