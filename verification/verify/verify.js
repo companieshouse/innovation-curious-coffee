c8c70cc617a709e5f9f78f28edeacd7b588b22fc
drosser <drosser@users.noreply.github.com>
2019-05-02 14:17:17 +0100

    Move verification modules to verification folder

--- verification/verify/verify.js
+++ verification/verify/verify.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const config =  require('../config/config.js');
+const config =  require('../../config/config.js');
 const mongojs = require('mongojs');
 
 const db = mongojs(config.db.name, config.db.collections);
