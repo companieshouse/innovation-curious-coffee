7f4a56ce32ace38804b906221629a1f9cd167c8c
drosser <drosser@users.noreply.github.com>
2019-05-03 12:46:52 +0100

    Update matchEdit to use async/await

--- matching/match-edit/matchEdit.js
+++ matching/match-edit/matchEdit.js
@@ -4,29 +4,21 @@ const middleware = require('../../core/middleware/middleware');
 const Match = require('../../models/match');
 const Participant = require('../../models/participant');
 
-router.get('/:id', middleware, function(req, res) {
+router.get('/:id', middleware, get);
+
+async function get(req, res) {
 
     console.log('Getting match at id: ', req.params.id);
 
-    getMatch(req.params.id, function(err, match) {
+    let match = await getMatch(req.params.id);
+    let options = await getParticipantEmails();
 
-        if (err) {
-            console.log(err);
-            return res.redirect('/oops');
-        }
+    return res.render('match-edit', {match: match, options: options, messages: req.flash('info')});
+};
 
-        getParticipantEmails(function(err, options) {
-            if (err) {
-                console.log(err);
-                return res.redirect('/oops');
-            }
+router.post('/:id', middleware, post);
 
-            return res.render('match-edit', {match: match, options: options, messages: req.flash('info')});
-        });
-    });
-});
-
-router.post('/:id', middleware, function(req, res) {
+async function post(req, res) {
 
     console.log('Posting updated match at id: ', req.params.id);
 
@@ -49,55 +41,54 @@ router.post('/:id', middleware, function(req, res) {
         req.flash('info', "You cannot match people within the same department");
         return res.redirect('/match-edit/' + req.params.id);
     } else {
-        getMatch(req.params.id, function(err, match) {
-            if (err) {
-                console.log(err);
-                return res.redirect('/oops');
-            }
+        let match = await getMatch(req.params.id);
 
-            match.person_1.name = match1.name;
-            match.person_1.email = match1.email;
-            match.person_1.department = match1.department;
+        //remove old matches from list
+        await Participant.update({email: match.person_1.email}, {$pull: {matches: match.person_2.email}});
+        await Participant.update({email: match.person_2.email}, {$pull: {matches: match.person_1.email}});
 
-            match.person_2.name = match2.name;
-            match.person_2.email = match2.email;
-            match.person_2.department = match2.department;
+        match.person_1.name = match1.name;
+        match.person_1.email = match1.email;
+        match.person_1.department = match1.department;
 
-            match.save();
+        match.person_2.name = match2.name;
+        match.person_2.email = match2.email;
+        match.person_2.department = match2.department;
 
-            req.flash('info', 'Match updated successfully. Please check the matched list to ensure there are no duplicates as a result of your change.');
-            return res.redirect('/');
-        });
+        match.save();
+
+        //add new matches to lists
+        await Participant.update({email: match1.email}, {$push: {matches: match2.email}});
+        await Participant.update({email: match2.email}, {$push: {matches: match1.email}});
+
+        req.flash('info', 'Match updated successfully. Please check the matched list to ensure there are no duplicates as a result of your change.');
+        return res.redirect('/');
     }
-});
+};
 
-router.get('/data/:email', middleware, function(req, res) {
+router.get('/data/:email', middleware, getData);
+
+async function getData(req, res) {
 
     res.setHeader('Content-Type', 'application/json');
 
     console.log('Query fired, email: ', req.params.email);
 
-    getParticipant(req.params.email, function(err, participant) {
-        
-        if (err) {
-            console.log(err);
-            return res.redirect('/oops');
-        }
+    let participant = await getParticipant(req.params.email);
 
-        return res.send({data: participant});
-    });
-});
-
-function getMatch(id, callback) {
-    return Match.findById(id, callback);
-}
-
-function getParticipantEmails(callback) {
-    return Participant.find({verify: true}, 'email', callback);
+    return res.send({data: participant});
 };
 
-function getParticipant(email, callback) {
-    return Participant.findOne({email: email}, callback);
-}
+function getMatch(id) {
+    return Match.findById(id);
+};
+
+function getParticipantEmails() {
+    return Participant.find({verify: true}, 'email');
+};
+
+function getParticipant(email) {
+    return Participant.findOne({email: email});
+};
 
 module.exports = router;
