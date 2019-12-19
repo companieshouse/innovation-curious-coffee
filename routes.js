fd3179153ea857de6e5d570c94c794294aef9ccf
drosser <drosser@users.noreply.github.com>
2019-12-19 12:36:02 +0000

    Refactor error page and move to src

--- routes.js
+++ routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
 const index = require('./core/index/index');
+const oops = require('./src/error');
 
 const admin = require('./src/admin');
 const cleanup = require('./src/admin/cleanup');
 
 router.use('/', index);
+router.use('/oops', oops);
 
 router.use('/admin', admin);
 router.use('/admin/cleanup', cleanup);
