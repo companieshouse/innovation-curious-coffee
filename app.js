62b584abe79bb35dbee0ad57b07772b552233c5f
drosser <drosser@users.noreply.github.com>
2020-01-13 14:38:26 +0000

    Move match to admin and restructure

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const routes = require('./routes');
 
 const register = require('./registration/register/registerController');
 const deregister = require('./registration/deregister/deregister');
-const match = require('./matching/match/match');
 const participantsByDepartment = require('./stats/participants-by-department/participants-by-department');
 const dateParticipantsRegistered = require('./stats/date-participants-registered/dateParticipantsRegistered');
 const verify = require('./verification/verify/verify');
@@ -75,7 +74,6 @@ app.use('/', routes);
 app.use('/register', register);
 app.use('/deregister', deregister);
 
-app.use('/match', match);
 app.use('/participants-by-department', participantsByDepartment);
 app.use('/date-participants-registered', dateParticipantsRegistered);
 app.use('/verify', verify);
