7204b62398380ed6b5fd380905f7a5ecb51a782e
drosser <drosser@users.noreply.github.com>
2020-02-17 17:03:42 +0000

    use strict and linting

--- src/admin/participants/remove/remove.js
+++ src/admin/participants/remove/remove.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const Participant = require('../../../models/participant');
 
 async function get(req, res) {
@@ -5,25 +7,25 @@ async function get(req, res) {
     let options = await Participant.find({verify: true});
 
     return res.render('remove', {options: options});
-};
+}
 
 function post(req, res) {
 
     if (req.body.email === '' || req.body.email === undefined) {
-        error = new Error("Email undefined!");
-        console.error(error);
+        const err = new Error("Email undefined!");
+        console.error(err);
         return err;
     }
 
     Participant.deleteOne({email: req.body.email}, function(err) {
         if (err) {
-            console.error(error);
+            console.error(err);
             return err;
         }
     });
 
     res.redirect('/');
-};
+}
 
 module.exports.get = get;
 module.exports.post = post;
