eb8b420787cb467b3bd0bcf28e4aba386e9f6fd4
drosser <drosser@users.noreply.github.com>
2020-02-18 14:07:24 +0000

    Updated Winston logger and added morgan for HTTP

--- src/logger/LoggerFactory.ts
+++ src/logger/LoggerFactory.ts
@@ -4,13 +4,17 @@ class LoggerFactory {
 
     static create(): winston.Logger {
 
+        const colorizer = winston.format.colorize();
+
         return winston.createLogger({
             transports: [
                 new winston.transports.Console({
                     level: 'debug',
                     format: winston.format.combine(
                         winston.format.timestamp(),
-                        winston.format.colorize()
+                        winston.format.printf(({ level, message, timestamp }) => {
+                            return colorizer.colorize(level, `${timestamp} ${level}: ${message}`);
+                          })
                     )
                 })
             ]
