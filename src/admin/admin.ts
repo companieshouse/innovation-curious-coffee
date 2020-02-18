4aecc3562b13c082e3271243f2682c2f7884e315
drosser <drosser@users.noreply.github.com>
2020-02-18 11:35:21 +0000

    Update admin files as per new eslint rules

--- src/admin/admin.ts
+++ src/admin/admin.ts
@@ -1,18 +1,18 @@
 import config from '../config';
 import {Request, Response} from 'express';
 
-export function get(req: Request, res: Response) {
+export function get(req: Request, res: Response): void {
     return res.render('admin');
 }
 
-export function post(req: Request, res: Response) {
+export function post(req: Request, res: Response): void {
     if (req.body.password == config.admin.password) {
         res.locals.session.user = true;
         return res.redirect('/');
     } else {
         return res.render('admin', {
                 password: req.body.password,
-                password_error: "Incorrect password."
+                passwordError: "Incorrect password."
         });
     }
 }
