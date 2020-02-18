07d3de568239b7e417bf002a1fc7a191151f49fa
drosser <drosser@users.noreply.github.com>
2020-02-18 14:08:07 +0000

    Added logging to admin

--- src/admin/middleware.ts
+++ src/admin/middleware.ts
@@ -1,13 +1,19 @@
 import {Request, Response, NextFunction} from 'express';
+import logger from '../logger';
 
 export function validate(req: Request, res: Response, next: NextFunction): void {
 
     if (req.originalUrl.startsWith("/admin/")) {
+
+        logger.info("Validating session for admin routes");
+
         if (!req.session) {
+            logger.info("No session found, redirecting to: admin");
             return res.redirect('/admin');
         }
         
         if (!req.session.user) {
+            logger.info("No user found in session, redirecting to: admin");
             return res.redirect('/admin');
         }
     }
