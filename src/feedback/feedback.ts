0f8b431ed5a5b8f87b0d5958a9799cba6246567a
drosser <drosser@users.noreply.github.com>
2020-02-19 15:30:19 +0000

    Small refactor to feedback validation and add logging

--- src/feedback/feedback.ts
+++ src/feedback/feedback.ts
@@ -1,20 +1,22 @@
 import {Request, Response} from 'express';
 import Feedback, {InterfaceFeedback} from '../models/feedback';
+import logger from '../logger';
 
 export function get(req: Request, res: Response): void {
+    logger.info("Rendering page: feedback");
     return res.render('feedback', {
         feedbackModel: new Feedback()
     });
 }
 
 export async function post(req: Request, res: Response): Promise<void> {
-
-    req.checkBody('feedbackModel[email]', 'Please enter a valid email address').isEmail();
-    req.checkBody('feedbackModel[feedback]', 'Please enter some feedback').isLength({min:1}).trim();
+    logger.info("Attempting to save feedback");
 
     const errors = req.validationErrors();
 
     if (errors) {
+
+        logger.info("Errors in feedback data, rendering page: feedback");
 
         return res.render('feedback', {
             feedbackModel: req.body.feedbackModel,
@@ -22,12 +24,16 @@ export async function post(req: Request, res: Response): Promise<void> {
         });
     } else {
 
+        logger.info("Saving feedback");
+
         const feedback: InterfaceFeedback = new Feedback({
             email: req.body.feedbackModel.email,
             feedback: req.body.feedbackModel.feedback
         });
 
         feedback.save();
+
+        logger.info("Feedback saved");
 
         req.flash('info', 'Thank you for your feedback!');
         return res.redirect('/');
