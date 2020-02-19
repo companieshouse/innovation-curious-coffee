55b1910ff71c3e5d02978a507f82976a78e66514
drosser <drosser@users.noreply.github.com>
2020-02-19 15:29:49 +0000

    Add logging to error and faq pages

--- src/error/error.ts
+++ src/error/error.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from 'express';
+import logger from '../logger';
 
 export function get(req: Request, res: Response): void {
+    logger.info("Rendering page: oops");
     return res.render('oops');
 }
