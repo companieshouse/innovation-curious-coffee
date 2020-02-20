b1fa90229a817693f3370742203c15964633e88b
drosser <drosser@users.noreply.github.com>
2020-02-20 12:09:11 +0000

    Add logging to verify

--- src/verify/verify.ts
+++ src/verify/verify.ts
@@ -3,14 +3,17 @@ import aws from 'aws-sdk';
 
 import config from '../config';
 import Participant from '../models/participant';
+import logger from '../logger';
 
 aws.config.update({region: 'eu-west-1'});
 
 export async function get(req: Request, res: Response): Promise<void> {
+    logger.info("Attempting to verify email address");
 
     //get email to verify
     const decode = req.params;
 
+    logger.info("Decoding email");
     //decode the string from base64 encoded
     const decoded = Buffer.from(decode.email, 'base64').toString('utf8');
 
@@ -19,15 +22,24 @@ export async function get(req: Request, res: Response): Promise<void> {
     const decodedEmail = decoded.slice(0, decoded.length - config.verify.signature.length);
 
     if (decodedEmail.length == 0) {
+        logger.info("Email is empty, redirecting to /");
         return res.redirect('/');
     }
 
     if (decodedSignature.length == 0) {
+        logger.info("Signature is empty, redirecting to /");
         return res.redirect('/');
     }
 
     if (decodedSignature == config.verify.signature) {
-        await Participant.updateOne({email: decodedEmail}, {$set: {verify: true}});
+        logger.info("Verification confirmed, updating participant info");
+        await Participant.updateOne({
+            email: decodedEmail
+        }, {
+            $set: {
+                verify: true
+            }
+        });
         
         const params = {
             Destination: {
@@ -52,8 +64,9 @@ export async function get(req: Request, res: Response): Promise<void> {
 
         const sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
     
-        sendPromise.then(function(data) {
-            console.log(data);
+        sendPromise.then(function(data: aws.SES.Types.SendEmailResponse) {
+            logger.info("Email sent, response: " + data);
+            
             req.flash('info', 'Congratulations! You have signed up for #CuriousCoffee. You will receive an email in due course matching you for your #CuriousCoffee');
             return res.redirect('/');
         }).catch(function(err) {
