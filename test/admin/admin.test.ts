1e05bee92ab5960d98b8b7ff15f5559d0f9c282e
drosser <drosser@users.noreply.github.com>
2020-02-20 19:10:12 +0000

    Add full admin tests, add sinon-chai dependency

--- test/admin/admin.test.ts
+++ test/admin/admin.test.ts
@@ -1,10 +1,82 @@
 import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import {Request, Response} from 'express';
+import {get, post} from '../../src/admin/admin';
+
 const expect = chai.expect;
+chai.use(sinonChai);
 
-describe("foo", function() {
-    it("bar", function() {
-        const f = "example";
+describe("admin", function () {
 
-        expect(f).to.equal("example");
-    })
+    describe("#get()", function () {
+        it("tries to render the admin page", function () {
+            const mockRequest: Partial<Request> = {
+            };
+
+            const mockResponse: Partial<Response> = {
+                render: sinon.spy()
+            };
+
+            get(
+                mockRequest as Request,
+                mockResponse as Response
+            );
+
+            expect(mockResponse.render).to.have.been.calledOnce;
+            expect(mockResponse.render).to.have.been.calledWith("admin");
+        });
+    });
+
+    describe("#post()", function () {
+        it("sets the session user to true when the correct password is entered", function () {
+            const mockRequest: Partial<Request> = {
+                body: {
+                    password: "test"
+                }
+            };
+
+            const mockResponse: Partial<Response> = {
+                locals: {
+                    session: {
+                        user: sinon.spy()
+                    },
+                },
+                redirect: sinon.spy()
+            };
+
+            post(
+                mockRequest as Request,
+                mockResponse as Response
+            );
+
+            expect(mockResponse.locals.session.user).to.equal(true);
+            expect(mockResponse.redirect).to.have.been.calledOnce;
+            expect(mockResponse.redirect).to.have.been.calledWith("/");
+        });
+
+        it("renders the admin page, setting the provided password and suitable error", function () {
+            const mockRequest: Partial<Request> = {
+                body: {
+                    password: "foo"
+                }
+            };
+
+            const mockResponse: Partial<Response> = {
+                render: sinon.spy()
+            };
+
+            post(
+                mockRequest as Request,
+                mockResponse as Response
+            );
+
+            expect(mockResponse.render).to.have.been.calledOnce;
+            expect(mockResponse.render).to.have.been.calledWith("admin", {
+                password: mockRequest.body.password,
+                passwordError: "Incorrect password."
+            });
+        });
+    });
 });
