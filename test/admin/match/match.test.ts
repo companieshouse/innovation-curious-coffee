ac9f1d410d67cd380f7f02f027ec499513646161
drosser <drosser@users.noreply.github.com>
2020-02-20 19:17:49 +0000

    Add first match test and update test command

---
+++ test/admin/match/match.test.ts
@@ -0,0 +1,28 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import {Request, Response} from 'express';
+
+import {get, post} from '../../../src/admin/match/match';
+
+const expect = chai.expect;
+chai.use(sinonChai);
+
+describe("#get()", function () {
+    it("tries to render the match page", function () {
+        const mockRequest: Partial<Request> = {
+        };
+
+        const mockResponse: Partial<Response> = {
+            render: sinon.spy()
+        };
+
+        get(
+            mockRequest as Request,
+            mockResponse as Response
+        );
+
+        expect(mockResponse.render).to.have.been.calledOnce;
+        expect(mockResponse.render).to.have.been.calledWith("match");
+    });
+});
