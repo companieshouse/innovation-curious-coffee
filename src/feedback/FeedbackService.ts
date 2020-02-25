cbf586557f865e1a6e1dff665601c8296d917372
drosser <drosser@users.noreply.github.com>
2020-02-25 11:59:47 +0000

    Fleshing out FeedbackService

--- src/feedback/FeedbackService.ts
+++ src/feedback/FeedbackService.ts
@@ -1,42 +1,52 @@
 import {Request, Response} from 'express';
 import Feedback, {InterfaceFeedback} from '../models/feedback';
 import logger from '../logger';
-import FeedbackRepository, {InterfaceFeedbackRepository} from '../repository/FeedbackRepository';
+import {InterfaceFeedbackRepository} from '../repository/FeedbackRepository';
 
 export default class FeedbackService {
-    get(req: Request, res: Response): void {
-        logger.info("Rendering page: feedback");
-        return res.render('feedback');
+
+    private repository: InterfaceFeedbackRepository;
+
+    constructor(repository: InterfaceFeedbackRepository) {
+        this.repository = repository;
     }
 
-    async post(req: Request, res: Response): Promise<void> {
-        logger.info("Attempting to save feedback");
-
+    private validateData(req: Request, res: Response): void {
         const errors = req.validationErrors();
 
         if (errors) {
-
             logger.info("Errors in feedback data, rendering page: feedback");
-
             return res.render('feedback', {
                 feedbackModel: req.body.feedbackModel,
                 errors: errors
             });
         } else {
-
-            logger.info("Saving feedback");
-
-            const feedback: InterfaceFeedback = new Feedback({
-                email: req.body.feedbackModel.email,
-                feedback: req.body.feedbackModel.feedback
-            });
-
-            feedback.save();
-
-            logger.info("Feedback saved");
-
-            req.flash('info', 'Thank you for your feedback!');
-            return res.redirect('/');
+            return;
         }
+    }
+
+    public get(req: Request, res: Response): void {
+        logger.info("Rendering page: feedback");
+        return res.render('feedback');
+    }
+
+    public async post(req: Request, res: Response): Promise<void> {
+        logger.info("Attempting to save feedback");
+
+        this.validateData(req, res);
+
+        logger.info("Saving feedback");
+
+        const feedback: InterfaceFeedback = new Feedback({
+            email: req.body.feedbackModel.email,
+            feedback: req.body.feedbackModel.feedback
+        });
+
+        feedback.save();
+
+        logger.info("Feedback saved");
+
+        req.flash('info', 'Thank you for your feedback!');
+        return res.redirect('/');
     }
 }
--- src/feedback/index.ts
+++ src/feedback/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import FeedbackService from './FeedbackService';
+import FeedbackRepository from '../repository/FeedbackRepository';
 import validation from './validation';
 
 const router = express.Router();
 
-const feedbackService = new FeedbackService();
+const feedbackService = new FeedbackService(new FeedbackRepository());
 
 router.get('/', feedbackService.get);
 router.post('/', validation, feedbackService.post);
