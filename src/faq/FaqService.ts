c8fb0553c6eff6f12187fd5d5b0f0a548bee8cc4
drosser <drosser@users.noreply.github.com>
2020-02-25 15:16:04 +0000

    Faq refactor

---
+++ src/faq/FaqService.ts
@@ -0,0 +1,9 @@
+import {Request, Response} from 'express';
+import logger from '../logger';
+
+export default class FaqService {
+    get = (req: Request, res: Response): void => {
+        logger.info("Rendering page: faq");
+        return res.render('faq');
+    }
+}
