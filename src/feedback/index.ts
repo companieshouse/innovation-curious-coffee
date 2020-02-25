02da92a5cb43dee1c31abb2a60f4d6e39e3c3f53
drosser <drosser@users.noreply.github.com>
2020-02-25 12:41:26 +0000

    More Feedback refactoring

--- src/feedback/index.ts
+++ src/feedback/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import FeedbackService from './FeedbackService';
 import FeedbackRepository from '../repository/FeedbackRepository';
-import validation from './validation';
+import validation, {checkValidation} from './validation';
 
 const router = express.Router();
 
 const feedbackService = new FeedbackService(new FeedbackRepository());
 
 router.get('/', feedbackService.get);
-router.post('/', validation, feedbackService.post);
+router.post('/', validation, checkValidation, feedbackService.post);
 
 export = router;
