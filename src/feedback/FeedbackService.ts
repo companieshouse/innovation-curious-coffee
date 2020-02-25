630ae01cced77c512dd19e43f1f12b9e673b08f8
drosser <drosser@users.noreply.github.com>
2020-02-25 13:02:33 +0000

    Fix this context problems

--- src/feedback/FeedbackService.ts
+++ src/feedback/FeedbackService.ts
@@ -1,34 +1,24 @@
 import {Request, Response} from 'express';
-import Feedback, {InterfaceFeedback} from '../models/feedback';
 import logger from '../logger';
 import {InterfaceFeedbackRepository} from '../repository/FeedbackRepository';
 
 export default class FeedbackService {
 
-    private repository: InterfaceFeedbackRepository;
+    private _repository: InterfaceFeedbackRepository;
 
     constructor(repository: InterfaceFeedbackRepository) {
-        this.repository = repository;
+        this._repository = repository;
     }
 
-    public get(req: Request, res: Response): void {
+    public get = (req: Request, res: Response): void => {
         logger.info("Rendering page: feedback");
         return res.render('feedback');
     }
 
-    public async post(req: Request, res: Response): Promise<void> {
+    public post = async (req: Request, res: Response): Promise<void> => {
         logger.info("Attempting to save feedback");
 
-        logger.info("Saving feedback");
-
-        const feedback: InterfaceFeedback = new Feedback({
-            email: req.body.feedbackModel.email,
-            feedback: req.body.feedbackModel.feedback
-        });
-
-        feedback.save();
-
-        logger.info("Feedback saved");
+        this._repository.save(req.body.feedbackModel.email, req.body.feedbackModel.feedback);
 
         req.flash('info', 'Thank you for your feedback!');
         return res.redirect('/');
--- src/repository/FeedbackRepository.ts
+++ src/repository/FeedbackRepository.ts
@@ -1,16 +1,21 @@
 import Feedback, {InterfaceFeedback} from '../models/feedback';
+import logger from '../logger';
 
 export interface InterfaceFeedbackRepository {
     save(email: string, feedback: string): void;
 }
 
 export default class FeedbackRepository implements InterfaceFeedbackRepository {
-    save(email: string, feedback: string): void {
+    public save = (email: string, feedback: string): void => {
+        logger.info("Saving feedback");
+
         const feedbackToSave: InterfaceFeedback = new Feedback({
             email: email,
             feedback: feedback
         });
 
         feedbackToSave.save();
+
+        logger.info("Feedback saved");
     }
 }
