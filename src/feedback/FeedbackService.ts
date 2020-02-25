301a111210fd1e2ed46eea7e8a9e3d42e09d69f3
drosser <drosser@users.noreply.github.com>
2020-02-25 13:06:24 +0000

    Move Repository and model into feedback

--- src/feedback/FeedbackService.ts
+++ src/feedback/FeedbackService.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from 'express';
 import logger from '../logger';
-import {InterfaceFeedbackRepository} from '../repository/FeedbackRepository';
+import {InterfaceFeedbackRepository} from './FeedbackRepository';
 
 export default class FeedbackService {
 
