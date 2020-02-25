f640447e56b92ad86a6aed63db4c833a72ee16c3
drosser <drosser@users.noreply.github.com>
2020-02-25 11:19:19 +0000

    Refactor methods into FeedbackService

--- src/feedback/index.ts
+++ src/feedback/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import {get, post} from './feedback';
+import FeedbackService from './FeedbackService';
 import validation from './validation';
 
 const router = express.Router();
 
-router.get('/', get);
-router.post('/', validation, post);
+const feedbackService = new FeedbackService();
+
+router.get('/', feedbackService.get);
+router.post('/', validation, feedbackService.post);
 
 export = router;
